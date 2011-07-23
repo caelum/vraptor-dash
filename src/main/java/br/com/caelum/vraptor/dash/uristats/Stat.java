24c5e358389d86d7a9f2ca9ca8306ac23e680d4a
Guilherme Silveira <guilherme.silveira@caelum.com.br>
2011-07-23 19:39:16 -0300

    mais fields

--- src/main/java/br/com/caelum/vraptor/dash/uristats/Stat.java
+++ src/main/java/br/com/caelum/vraptor/dash/uristats/Stat.java
@@ -23,26 +23,43 @@ public class Stat {
 	
 	private final String userId;
 	
-	private Calendar createdAt;
+	private final Calendar createdAt;
 	
-	private String verb;
+	private final String verb;
 	
-	private String resource;
+	private final String resource;
 	
-	private String method;
+	private final String method;
 
 	private final long time;
 	
+	private final String etag;
+	
+	private final int resultCode;
+
+	private final boolean hadEtag;
+
+	private final String cache;
+
+	private final int size;
+	
 	Stat() {
-		this("", "", 0, "", "", "");
+		this("", "", 0, "", "", "", "",0 , false, "", 0);
 	}
 	
-	public Stat(String userId, String uri, long time, String verb, String resource, String action) {
+	public Stat(String userId, String uri, long time, String verb, String resource, String action, String etag, int resultCode, boolean hadEtag, String cache, int size) {
 		this.userId = userId;
 		this.uri = uri;
 		this.time = time;
 		this.verb = verb;
+		this.resultCode = resultCode;
+		this.hadEtag = hadEtag;
+		this.cache = cache;
+		this.size = size;
 		this.createdAt = Calendar.getInstance();
+		this.resource = resource;
+		this.method = action;
+		this.etag = etag;
 	}
 
 }
--- src/main/java/br/com/caelum/vraptor/dash/uristats/URIStatInterceptor.java
+++ src/main/java/br/com/caelum/vraptor/dash/uristats/URIStatInterceptor.java
@@ -1,12 +1,14 @@
 package br.com.caelum.vraptor.dash.uristats;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.hibernate.Session;
 
 import br.com.caelum.vraptor.InterceptionException;
 import br.com.caelum.vraptor.Intercepts;
 import br.com.caelum.vraptor.core.InterceptorStack;
+import br.com.caelum.vraptor.dash.cache.ObservableResponse;
 import br.com.caelum.vraptor.interceptor.ExecuteMethodInterceptor;
 import br.com.caelum.vraptor.interceptor.Interceptor;
 import br.com.caelum.vraptor.ioc.Container;
@@ -23,12 +25,14 @@ public class URIStatInterceptor implements Interceptor {
 	private final Session session;
 	private final HttpServletRequest request;
 	private final Container container;
+	private final HttpServletResponse response;
 
 	public URIStatInterceptor(Container container, Session session,
-			HttpServletRequest request) {
+			HttpServletRequest request, HttpServletResponse response) {
 		this.container = container;
 		this.session = session;
 		this.request = request;
+		this.response = response;
 	}
 
 	public boolean accepts(ResourceMethod arg0) {
@@ -41,6 +45,28 @@ public class URIStatInterceptor implements Interceptor {
 		stack.next(method, instance);
 		long time = System.currentTimeMillis() - before;
 
+		String key = userKey(container);
+
+		String resource = method.getResource().getType().getName();
+		String methodName = method.getMethod().getName();
+
+		String etag = response.getHeader("etag");
+		boolean hadEtag = etag != null;
+		int size = 0;
+		if (response instanceof ObservableResponse && !hadEtag) {
+			ObservableResponse resp = (ObservableResponse) response;
+			etag = resp.getMd5();
+			size = resp.getBufferSize();
+		}
+
+		Stat stat = new Stat(key, request.getRequestURI(), time,
+				request.getMethod(), resource, methodName,
+				response.getHeader("etag"), response.getStatus(), hadEtag,
+				response.getHeader("cache-control"), size);
+		session.save(stat);
+	}
+
+	public static String userKey(Container container) {
 		String key = "";
 		try {
 			IdeableUser user = container.instanceFor(IdeableUser.class);
@@ -48,13 +74,7 @@ public class URIStatInterceptor implements Interceptor {
 		} catch (Exception e) {
 			key = "";
 		}
-
-		String resource = method.getResource().getType().getName();
-		String actionName = method.getMethod().getName();
-		
-		Stat stat = new Stat(key, request.getRequestURI(),
-				time, request.getMethod(), resource, actionName);
-		session.save(stat);
+		return key;
 	}
 
 }
