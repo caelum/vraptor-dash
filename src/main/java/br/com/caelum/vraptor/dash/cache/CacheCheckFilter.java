5b644fda970d4b89be4193794ebbdd49f71537d1
Guilherme Silveira <guilherme.silveira@caelum.com.br>
2011-07-23 19:41:08 -0300

    cache stats

---
+++ src/main/java/br/com/caelum/vraptor/dash/cache/CacheCheckFilter.java
@@ -0,0 +1,37 @@
+package br.com.caelum.vraptor.dash.cache;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.log4j.Logger;
+
+public class CacheCheckFilter implements Filter {
+
+	private final static Logger LOGGER = Logger.getLogger(CacheCheckFilter.class);
+
+	public void destroy() {
+	}
+
+	public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException,
+			ServletException {
+		try {
+			res = new ObservableResponse((HttpServletResponse) res);
+		} catch (Exception ex) {
+			LOGGER.error("Observable response was not injected", ex);
+		}
+
+		chain.doFilter(req, res);
+
+	}
+
+	public void init(FilterConfig arg0) throws ServletException {
+	}
+
+}
---
+++ src/main/java/br/com/caelum/vraptor/dash/cache/ObservableResponse.java
@@ -0,0 +1,128 @@
+package br.com.caelum.vraptor.dash.cache;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpServletResponseWrapper;
+
+public class ObservableResponse extends HttpServletResponseWrapper {
+
+	private MessageDigest digester;
+
+	public ObservableResponse(HttpServletResponse res) {
+		super(res);
+		try {
+			this.digester = MessageDigest.getInstance("MD5");
+		} catch (NoSuchAlgorithmException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	private PrintWriter writer;
+	private ResponseServletOutputStream oStream;
+	private boolean isCaching = false;
+
+	public ServletOutputStream getOutputStream() throws IOException {
+		if (this.oStream == null) {
+			this.oStream = new ResponseServletOutputStream(super.getOutputStream());
+		}
+		return this.oStream;
+	}
+
+	public PrintWriter getWriter() throws IOException {
+		if (this.writer == null) {
+			this.writer = new ResponsePrintWriter(super.getWriter());
+		}
+		return this.writer;
+	}
+
+	@Override
+	public void resetBuffer() {
+		try {
+			this.digester = MessageDigest.getInstance("MD5");
+		} catch (NoSuchAlgorithmException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	@Override
+	public void reset() {
+		super.reset();
+		resetBuffer();
+	}
+
+	public String getMd5() {
+		// GAMBI WIN
+		String c = "";
+		for (byte b : this.digester.digest()) {
+			c += (int) b;
+		}
+		return c;
+	}
+
+	private int size;
+
+	private class ResponseServletOutputStream extends ServletOutputStream {
+
+		private final ServletOutputStream original;
+
+		public ResponseServletOutputStream(ServletOutputStream outputStream) {
+			this.original = outputStream;
+		}
+
+		@Override
+		public void write(int b) throws IOException {
+			original.write(b);
+			digester.update((byte) b);
+			size++;
+		}
+
+		@Override
+		public void write(byte[] b, int off, int len) throws IOException {
+			original.write(b, off, len);
+			digester.update(b, off, len);
+			size += len;
+		}
+
+	}
+
+	private class ResponsePrintWriter extends PrintWriter {
+
+		private ResponsePrintWriter(PrintWriter printWriter) {
+			super(printWriter);
+		}
+
+		@Override
+		public void write(char buf[], int off, int len) {
+			super.write(buf, off, len);
+			super.flush();
+			size += len;
+			digester.update(new String(buf).getBytes());
+		}
+
+		@Override
+		public void write(String s, int off, int len) {
+			super.write(s, off, len);
+			super.flush();
+			size += len;
+			digester.update(s.getBytes());
+		}
+
+		@Override
+		public void write(int c) {
+			size++;
+			digester.update((byte) c);
+			super.write(c);
+			super.flush();
+		}
+
+	}
+
+	public long size() {
+		return size;
+	}
+}
