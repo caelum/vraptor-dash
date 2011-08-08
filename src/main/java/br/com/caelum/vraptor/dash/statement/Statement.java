c522e3e93bbfaa9ba5da239abafaf8ed3a370aac
William Seiti Mizuta & Luiz Fernando Oliveira Corte Real <sr.saude@gmail.com>
2011-08-08 15:42:24 -0300

    Putting @Lob in the uri field in Stat and the hql field in Statement

--- src/main/java/br/com/caelum/vraptor/dash/statement/Statement.java
+++ src/main/java/br/com/caelum/vraptor/dash/statement/Statement.java
@@ -7,6 +7,7 @@ import java.util.StringTokenizer;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.Lob;
 import javax.persistence.Table;
 
 import org.hibernate.annotations.GenericGenerator;
@@ -26,6 +27,7 @@ public class Statement {
 
 	private String name;
 
+	@Lob
 	private String hql;
 
 	private String password;
--- src/main/java/br/com/caelum/vraptor/dash/uristats/Stat.java
+++ src/main/java/br/com/caelum/vraptor/dash/uristats/Stat.java
@@ -5,6 +5,7 @@ import java.util.Calendar;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.Lob;
 import javax.persistence.Table;
 
 import org.slf4j.Logger;
@@ -17,27 +18,28 @@ import org.slf4j.LoggerFactory;
 @Table(name="DashUriStat")
 @Entity(name="DashUriStat")
 public class Stat {
-	
+
 	@Id
 	@GeneratedValue
 	private Long id;
-	
+
+	@Lob
 	private final String uri;
-	
+
 	private final String userId;
-	
+
 	private final Calendar createdAt;
-	
+
 	private final String verb;
-	
+
 	private final String resource;
-	
+
 	private final String method;
 
 	private final long time;
-	
+
 	private final String etag;
-	
+
 	private final int resultCode;
 
 	private final String hadEtag;
@@ -45,11 +47,11 @@ public class Stat {
 	private final String cache;
 
 	private final long size;
-	
+
 	Stat() {
 		this("", "", 0, "", "", "", "",0 , "unknown", "", 0);
 	}
-	
+
 	public Stat(String userId, String uri, long time, String verb, String resource, String action, String etag, int resultCode, String hadEtag, String cache, long size2) {
 		this.userId = userId;
 		this.uri = uri;
@@ -66,7 +68,7 @@ public class Stat {
 	}
 
 	private static final Logger LOG = LoggerFactory.getLogger(Stat.class);
-	
+
 	public void log() {
 		LOG.debug(String.format("%tc-[%s]-%s-%s-%s-%d-%s-%d-[%s]-%d-[%s]-%s", createdAt,etag, verb, resource, method, time, hadEtag, resultCode, cache, size, userId, uri));
 	}
